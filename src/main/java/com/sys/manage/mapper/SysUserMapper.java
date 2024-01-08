585bf2f513e081a45b2765f18c37a43fc0bfd887
我的 <1624532435@qq.com>
2024-01-08 14:14:18 +0800

    初建项目

---
+++ src/main/java/com/sys/manage/mapper/SysUserMapper.java
@@ -0,0 +1,18 @@
+package com.sys.manage.mapper;
+
+import com.sys.manage.entity.SysUser;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author 51430
+* @description 针对表【sys_user】的数据库操作Mapper
+* @createDate 2024-01-08 14:03:20
+* @Entity com.sys.manage.entity.SysUser
+*/
+public interface SysUserMapper extends BaseMapper<SysUser> {
+
+}
+
+
+
+
---
+++ src/main/java/com/sys/manage/service/impl/SysUserServiceImpl.java
@@ -0,0 +1,22 @@
+package com.sys.manage.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.sys.manage.entity.SysUser;
+import com.sys.manage.service.SysUserService;
+import com.sys.manage.mapper.SysUserMapper;
+import org.springframework.stereotype.Service;
+
+/**
+* @author 51430
+* @description 针对表【sys_user】的数据库操作Service实现
+* @createDate 2024-01-08 14:03:20
+*/
+@Service
+public class SysUserServiceImpl extends ServiceImpl<SysUserMapper, SysUser>
+    implements SysUserService{
+
+}
+
+
+
+

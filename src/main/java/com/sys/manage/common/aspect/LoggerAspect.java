55d259cebb81c76ec754cff2c62668b240809cf8
zhangjin <1624532435@qq.com>
2024-01-26 16:37:56 +0800

    加入切面日志功能

---
+++ src/main/java/com/sys/manage/common/aspect/LoggerAspect.java
@@ -0,0 +1,33 @@
+package com.sys.manage.common.aspect;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+import java.util.Objects;
+
+@Component
+@Slf4j
+@Aspect
+public class LoggerAspect {
+
+    @Pointcut("execution( * com.sys.manage.controller.*.*(..))")
+    public void myPointCut(){
+
+    }
+
+    @Around("myPointCut()")
+    public Object myLogger(ProceedingJoinPoint pjp) throws Throwable {
+        String className = Objects.requireNonNull(pjp.getTarget()).getClass().toString();
+        String methodName = pjp.getSignature().getName();
+        Object[] args = pjp.getArgs();
+        log.info("调用接口为"+className+":"+methodName+":"+new ObjectMapper().writeValueAsString(args));
+        Object obj = pjp.proceed();
+        log.info("返回结果为"+":"+new ObjectMapper().writeValueAsString(obj));
+        return obj;
+    }
+}
